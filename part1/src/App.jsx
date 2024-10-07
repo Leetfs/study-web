b4e51aa5ea48630db69ca16a3def4ae22413b858
Lee <lee@mtftm.com>
2024-10-08 01:40:28 +0800

    add part1

---
+++ part1/src/App.jsx
@@ -0,0 +1,24 @@
+const Hello = (props) => {
+  return (
+    <div>
+      <p>你好，{props.name}，你的年龄：{props.age}</p>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const name = "Lee"
+  const age = 9+10
+  
+
+  return (
+    <div>
+      <h1>你好世界</h1>
+      <Hello name = {name}  age = {age}  />
+      <Hello name="github" age={"不知道"} />
+    </div>
+  )
+}
+
+export default App
